9f1f16aeb526f4d296a8b69b4e05ad3c75a31945
Asif Ahmed <asif@stitchdesignco.com>
2017-12-06 09:21:16 -0500

    move head to partial 'anchor'

---
+++ content/themes/Hay-EGWLF17/components/head/anchors.php
@@ -0,0 +1,29 @@
+<?php
+  /*
+  * Section =>  Anchors
+  */
+
+	$seal_logo = file_get_contents($GLOBALS['url']."/assets/svg/svg_seal.svg");
+	$main_logo = file_get_contents($GLOBALS['url']."/assets/svg/svg_haymaker-logo.svg");
+	$hamburger_icon = file_get_contents($GLOBALS['url']."/assets/svg/svg_hamburger.svg");
+	$menu_repeater = get_field('menu', 'options');
+	$reserve = $menu_repeater[2]['link']; ?>
+
+
+<header class="head-anchors">
+  <div class="nav top-left">
+    <a href=""><?php echo $seal_logo; ?></a>
+  </div>
+  <div class="nav top-middle">
+    <a href=""><?php echo $main_logo;  ?></a>
+  </div>
+  <div class="nav top-right">
+    <a id="view-menu" href=""><?php echo $hamburger_icon; ?></a>
+  </div>
+  <div class="nav bottom-left">
+    <a id="view-contact" href="#">Contact</a>
+  </div>
+  <div class="nav bottom-right">
+    <a href="<?php echo $reserve['url']; ?>" target="_blank">Reserve</a>
+  </div>
+</header>
