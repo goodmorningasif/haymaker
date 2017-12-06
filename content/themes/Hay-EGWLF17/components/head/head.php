0e6abbb79360cfdeb7c56f91c5fe7044e8f113e3
Asif Ahmed <asif@stitchdesignco.com>
2017-12-06 09:51:45 -0500

    add pre-comp component to header

--- content/themes/Hay-EGWLF17/components/head/head.php
+++ content/themes/Hay-EGWLF17/components/head/head.php
@@ -3,6 +3,12 @@
   * Section =>  Head
   */
 
+  $head = get_field('head');
+
+  if($head && $head[0] === 'Enable'):
+    include(locate_template('components/head/pre-comp.php'));  
+  endif;
+
   include(locate_template('components/head/anchors.php'));
 
   ?>
---
+++ content/themes/Hay-EGWLF17/components/head/pre-comp.php
@@ -0,0 +1,18 @@
+<?php
+  /*
+  * Section =>  Pre-Comp
+  */
+
+	$assets = get_field('assets'); 
+  $image = $assets['image']; ?>
+
+<div class="pre-comp">
+	<div class="text-block">
+		<?php echo get_field('text'); ?>
+	</div>
+	<div class="assets" 
+	  style="background-image: url('<?php echo $image['url']; ?>'); 
+		max-width: <?php echo $image['width']; ?>px;
+		height: <?php echo $image['height'] ?>px;">
+	</div>
+</div>
